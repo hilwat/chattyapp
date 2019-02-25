b4ce1d7aa323707557371db0c9c80ff029cd6e1e
Hilary <watson.hilary@gmail.com>
2019-02-25 13:13:55 -0500

    fixed set state so per user for name update

--- src/App.jsx
+++ src/App.jsx
@@ -26,81 +26,78 @@ export default class App extends Component {
   }
 
   sendMessage = (event) => {
-	if(event.key === 'Enter') {
-	  this.socket.send(JSON.stringify({ 
-			username: this.state.currentUser.name, 
-			content: event.target.value,
-			type:"incomingMessage"
+		if(event.key === 'Enter') {
+			this.socket.send(JSON.stringify({ 
+				username: this.state.currentUser.name, 
+				content: event.target.value,
+				type:"incomingMessage"
+			}));
+			event.target.value = "";
 		}
-		));
-	  event.target.value = "";
-	}
   }
   
   updateUsername = (event) => {
-	if (event.key === 'Enter') {
-	this.socket.send(JSON.stringify({ 
-		username: event.target.value, 
-		previousname: this.state.currentUser.name,
-		type:"incomingNotification"
+		if (event.key === 'Enter') {
+			this.socket.send(JSON.stringify({ 
+				username: event.target.value, 
+				previousname: this.state.currentUser.name,
+				type:"incomingNotification"
+			}));
+			this.setState({ 
+				currentUser: { name: event.target.value }
+			});
 		}
-	  ));
-  	}
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!", type:"incomingMessage"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
-    this.socket = new WebSocket('ws://localhost:3001')
-	
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+		// Add a new message to the list of messages in the data store
+		const newMessage = {id: 3, username: "Michelle", content: "Hello there!", type:"incomingMessage"};
+		const messages = this.state.messages.concat(newMessage)
+		// Update the state of the app component.
+		// Calling setState will trigger a call to render() in App and all child components.
+		this.setState({messages: messages})
+	}, 3000);
+		
+  this.socket = new WebSocket('ws://localhost:3001')
 	this.socket.onopen = () => {
       console.log('Connected 3001');
-    }
+  }
 	
 	this.socket.onmessage = (event) => {
 		let data = JSON.parse(event.data)
-		console.log(data)
-      	switch(data.type) {
-		case "incomingMessage":
-			var currentMessages = this.state.messages;
-		    currentMessages.push(data);
-		    this.setState({ 
-		    messages: currentMessages
-		    });
-      break;
-    case "incomingNotification":
-			var currentMessages = this.state.messages;
-			currentMessages.push(data);
-			this.setState({ 
-		  	messages: currentMessages, currentUser: { name: data.username }
-			});
-		  break;
-		case "userUpdate":
-			this.setState({usersOnline: data.counter});
-			break;
-		default:
-        throw new Error("Unknown event type " + data.type);
-      }
+    switch(data.type) {
+			case "incomingMessage":
+				var currentMessages = this.state.messages;
+					currentMessages.push(data);
+					this.setState({ 
+					messages: currentMessages
+					});
+				break;
+			case "incomingNotification":
+				var currentMessages = this.state.messages;
+				currentMessages.push(data);
+				break;
+			case "userUpdate":
+				this.setState({usersOnline: data.counter});
+				break;
+			default:
+				throw new Error("Unknown event type " + data.type);
+    }
   }
 }
 
   render() {
-      return (
-     <div>
-       <nav className="navbar">
-        <span href="/" className="navbar-brand">Chatty</span><span className="noOnline">Number Online: {this.state.usersOnline}</span>
-      </nav>
-      <MessageList messages={this.state.messages} />
-      <ChatBar username={this.state.currentUser.name} sendMessage={this.sendMessage} updateUsername={this.updateUsername}/>
-     </div>
-      );
+		return (
+		<div>
+			<nav className="navbar">
+				<span href="/" className="navbar-brand">Chatty</span><span className="noOnline">Number Online: {this.state.usersOnline}</span>
+			</nav>
+			<MessageList messages={this.state.messages} />
+			<ChatBar username={this.state.currentUser.name} sendMessage={this.sendMessage} updateUsername={this.updateUsername}/>
+		</div>
+		);
   }
 }
 
